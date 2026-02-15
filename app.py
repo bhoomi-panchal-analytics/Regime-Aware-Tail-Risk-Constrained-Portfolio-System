6bed77b5b415ca7c12f80e52b7b6bdd963f8983c
Bhoomi_Panchal_analytics <bhoomidpanchal@gmail.com>
2026-02-15 11:37:17 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.title("Regime-Aware Risk Allocation System")
+st.markdown("Macro → Volatility → Contagion → Survival vs Alpha")
+
+st.sidebar.success("Select a page above.")
