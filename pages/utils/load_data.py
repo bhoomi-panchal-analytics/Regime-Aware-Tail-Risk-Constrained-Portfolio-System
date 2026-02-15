e497a2fe3999575d3856163bc6c35ca30783662d
Bhoomi_Panchal_analytics <bhoomidpanchal@gmail.com>
2026-02-15 11:49:54 +0530

    Update load_data.py

--- pages/utils/load_data.py
+++ pages/utils/load_data.py
@@ -1,15 +1,43 @@
 import pandas as pd
+import os
 
-def load_csv(path):
-    return pd.read_csv(path, index_col=0, parse_dates=True)
+DATA_PATH = "data"
+
+def load_macro_from_excels():
+    macro_df = pd.DataFrame()
+    
+    for file in os.listdir(DATA_PATH):
+        if file.endswith(".xlsx") or file.endswith(".xls"):
+            
+            path = os.path.join(DATA_PATH, file)
+            df = pd.read_excel(path)
+            
+            # Standardize column names
+            df.columns = [col.strip() for col in df.columns]
+            
+            # Assume first column is date
+            df.iloc[:,0] = pd.to_datetime(df.iloc[:,0])
+            df.set_index(df.columns[0], inplace=True)
+            
+            series_name = file.split(".")[0]
+            df.columns = [series_name]
+            
+            macro_df = pd.concat([macro_df, df], axis=1)
+    
+    macro_df.sort_index(inplace=True)
+    return macro_df
+
 
 def load_all():
     data = {}
-    data["macro"] = load_csv("data/macro_regime_matrix.csv")
-    data["regime_probs"] = load_csv("data/regime_probabilities.csv")
-    data["market"] = load_csv("data/market_prices.csv")
-    data["vix"] = load_csv("data/vix.csv")
-    data["garch"] = load_csv("data/garch_volatility.csv")
-    data["contagion"] = load_csv("data/contagion_index.csv")
-    data["metrics"] = load_csv("data/portfolio_metrics.csv")
+    
+    data["macro"] = load_macro_from_excels()
+    
+    # These must already exist as CSV
+    data["regime_probs"] = pd.read_csv("data/regime_probabilities.csv", index_col=0, parse_dates=True)
+    data["garch"] = pd.read_csv("data/garch_volatility.csv", index_col=0, parse_dates=True)
+    data["contagion"] = pd.read_csv("data/contagion_index.csv", index_col=0, parse_dates=True)
+    data["vix"] = pd.read_csv("data/vix_synthetic.csv", index_col=0, parse_dates=True)
+    data["metrics"] = pd.read_csv("data/portfolio_metrics.csv")
+    
     return data
